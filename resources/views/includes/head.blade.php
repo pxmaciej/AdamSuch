afeb2a416d3377953070bdd02d64fe5c996b7e75
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2022-01-23 23:44:08 +0100

    make it better

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,8 +5,8 @@
 <title>Szczypta Muzyki</title>
 
 <!-- Stylesheets -->
-<link href="{{ asset('css/style.css') }}" rel="stylesheet">
-<link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('css/responsive.css') }}" rel="stylesheet">
 <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
